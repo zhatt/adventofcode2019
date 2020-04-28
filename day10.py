4da834bb99c00d8d356bddf5b47e28db6d5739f8
Craig Robson <craig@zhatt.com>
2020-04-28 07:08:19 -0700

    Remove unneeded loop.
    Removed an outer while loop in simulation that only executed once.

--- day10.py
+++ day10.py
@@ -66,20 +66,19 @@ def simulate_destroying_asteroids(asteroid_map):
     best_location, _ = find_best_location(asteroid_map)
     slope_set = calculate_slopes(best_location, asteroid_map)
 
-    while num_asteroids_destroyed < 200:
-        clockwise_ordered_list = sorted(slope_set, reverse=True)
-        for slope in clockwise_ordered_list:
-            location_list = slope_set[slope]
+    clockwise_ordered_list = sorted(slope_set, reverse=True)
+    for slope in clockwise_ordered_list:
+        location_list = slope_set[slope]
 
-            location = location_list.pop(0)
-            if not location_list:
-                # All locations on this slope are used so drop it.
-                slope_set.pop(slope)
+        location = location_list.pop(0)
+        if not location_list:
+            # All locations on this slope are used so drop it.
+            slope_set.pop(slope)
 
-            num_asteroids_destroyed += 1
+        num_asteroids_destroyed += 1
 
-            if num_asteroids_destroyed == 200:
-                break
+        if num_asteroids_destroyed == 200:
+            break
 
     return location.location.x_val * 100 + location.location.y_val
 
